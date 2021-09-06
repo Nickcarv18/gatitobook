5772bbb3ee98d12adc8ad44ad48363f3027037a4
Nickcarv18 <nicolicarvalho1@hotmail.com>
2021-09-06 13:32:42 -0300

    Serviço de ActivatedRoute

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,9 +1,10 @@
 import { environment } from './../../environments/environment';
 import { TokenService } from './../autenticacao/token.service';
-import { Animais } from './animais';
+import { Animais, Animal } from './animais';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { $ } from 'protractor';
 
 const API = environment.apiURL;
 
@@ -23,5 +24,12 @@ export class AnimaisService {
     return this.httpClient.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
       headers,
     });
+  }
+
+  buscaPorId(id:number):Observable<Animal>{
+    const token = this.tokenServise.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+
+    return this.httpClient.get<Animal>(`${API}/photos/${id}`, {headers});
   }
 }
--- src/app/animais/datelhe-animal/datelhe-animal.component.ts
+++ src/app/animais/datelhe-animal/datelhe-animal.component.ts
@@ -1,15 +1,25 @@
+import { AnimaisService } from './../animais.service';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Animal } from '../animais';
 
 @Component({
   selector: 'app-datelhe-animal',
   templateUrl: './datelhe-animal.component.html',
-  styleUrls: ['./datelhe-animal.component.css']
+  styleUrls: ['./datelhe-animal.component.css'],
 })
 export class DatelheAnimalComponent implements OnInit {
+  animalId!: number;
+  animal$!: Observable<Animal>;
 
-  constructor() { }
+  constructor(
+    private animaisService: AnimaisService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.animalId = this.activatedRoute.snapshot.params.animalId;
+    this.animal$ = this.animaisService.buscaPorId(this.animalId);
   }
-
 }
